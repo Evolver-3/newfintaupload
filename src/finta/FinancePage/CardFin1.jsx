e6625a91e393d3fdfaed256047aaa33779382ca6
Ashu Singh <Ashusingh2201@gmail.com>
2026-02-03 00:44:34 +0530

    updation done but animation not working

--- src/finta/FinancePage/CardFin1.jsx
+++ src/finta/FinancePage/CardFin1.jsx
@@ -1,44 +1,28 @@
-import React from 'react'
+
 import NormalCardContain from '../NormalCardContain'
-import {motion} from 'motion/react'
 
 const CardFin1 = () => {
-  
-const MotionCard=motion(SmallCard)
+ 
+
   return (
  <NormalCardContain heading={"INVESTOR-READY METRICS"} text={"Send investor updates with real-time access to cash, burn rate, runway, and MRR."} logo={""}>
 
-      <div className='relative w-full h-40 '>
-      <div className=' relative flex items-center'>
+      <div className='relative w-full h-40 overflow-hidden'>
+      <div className=' relative flex w-72 my-4'>
         
-          <MotionCard
-          initial={{x:-200,opacity:0.5}}
-          animate={{x:100,opacity:1}}
-          transition={{duration:3}}
-          
-          
-          text={"data"} amount={"$4,333,232"} time={"Updated 1m ago"}>
+         <SmallCard  text={'Data'} amount={"$123,000"} time={"Updated 1m ago"} className='translate-x-0 opacity-100 z-1'/>
 
-          </MotionCard>
+         <SmallCard  text={'Data'} amount={"$123,000"} time={"Updated 1m ago"} className='translate-x-[calc(100%+16px)] translate-y-4'/>
 
-          <MotionCard
-          initial={{x:200,opacity:0.5}}
-          
-          
-          text={"data"} amount={"$4,333,232"} time={"Updated 1m ago"}>
+         <SmallCard  text={'Data'} amount={"$123,000"} time={"Updated 1m ago"} className='translate-x-[calc(-100%-16px)]
+'/>
 
-          </MotionCard>
+         <SmallCard  text={'Data'} amount={"$123,000"} time={"Updated 1m ago"} className='opacity-0 translate-x-[200%]'/>
 
-          <MotionCard
-          initial={{x:100,opacity:1}}
-          
-          
-          text={"data"} amount={"$4,333,232"} time={"Updated 1m ago"}>
+         <SmallCard  text={'Data'} amount={"$123,000"} time={"Updated 1m ago"} className='opacity-0 translate-x-[200%]'/>
 
-          </MotionCard>
-
-        
-
+         <SmallCard  text={'Data'} amount={"$123,000"} time={"Updated 1m ago"} className='opacity-0 translate-x-[200%]'/>
+         
        
       </div>
       </div>
@@ -49,19 +33,22 @@ const MotionCard=motion(SmallCard)
 export default CardFin1
 
 
-const SmallCard=({text,amount,time})=>{
+const SmallCard=({text,amount,time,className=''})=>{
 
   return(
     <div
-          className= 'rounded-md w-60 shrink-0'>
-            <div className='flex flex-col gap-4 shadow-finta rounded-xl'>
-              <h3>{text}</h3>
+          className= {`absolute inset-0 rounded-md w-full p-4 shrink-0 ${className}`}>
+            <div className='flex flex-col gap-3 shadow-weird rounded-xl py-2 px-4 '>
+              <h3 className='text-sm text-neutral-800 font-sans'>{text}</h3>
 
-              <h2>{amount}</h2>
+              <h2 className='text-[25px] leading-tight font-semibold'>{amount}</h2>
 
-              <div>{time}</div>
+              <div className='px-2 py-1 rounded-xl bg-neutral-200 text-sec text-neutral-500 w-fit'>{time}</div>
 
             </div>
           </div>
   )
 }
+
+
+//mask-x-from-90% mask-x-to-100%
