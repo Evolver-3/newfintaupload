6647e9f276cce3e62dbae0dd9cf22db5c0b828f4
Ashu Singh <Ashusingh2201@gmail.com>
2026-02-03 20:59:29 +0530

    added new cards

--- src/finta/Accounting/Card3/InnerCard3.jsx
+++ src/finta/Accounting/Card3/InnerCard3.jsx
@@ -1,5 +1,6 @@
 
-import React from 'react'
+import { motion } from 'motion/react'
+
 
 const dataItems=[
   {id:1,logo:"",text:"Brex",time:"12:03 AM",amount:"-$450,000"},
@@ -8,32 +9,71 @@ const dataItems=[
   {id:4,logo:"",text:"Jire",time:"02:45 AM",amount:"-$5,000"}
 ]
 
+const variants = {
+  hidden: (pos) => {
+    if (pos === 0) return { opacity: 0, y: -100 ,}
+    if (pos === 1) return { opacity: 0, y: 180 }
+    return { opacity: 0 }
+  },
+
+  visible: (pos) => {
+    if (pos === 0) return { opacity: 1, y: 0}
+    if (pos === 1) return { opacity: 1, y: 70 }
+    return { opacity: 0 }
+  }
+}
+
+
 
 const InnerCard3 = () => {
+
+
   return (
-    <div className='w-full h-fit container'>
-      <div className='grid grid-cols-1 place-items-center  '>
-        {dataItems.map((item,index)=>(
-          <div className='w-full shadow-finta flex gap-4 justify-around mx-5 items-center rounded-xl font-sans text-sm bg-white' key={item.index}>
+      <div className='relative w-full h-40 flex justify-center overflow-hidden'>
+      <div className=' relative w-72 my-4 h-full '>
 
-            <div className='flex gap-4 items-center'>
-              <div className='w-10 h-10 bg-lime-500 rounded-full'></div>
+        {dataItems.map((item,i)=> {
+          const pos=i;
 
-            <div className='flex flex-col gap-2 items-center'>
-              <h4 className='font-medium'>{item.text}</h4>
-              <h2 className='text-xs font-normal text-gray-500'>{item.time}</h2>
-            </div>
-              
-            </div>
-
-            <h4 className='font-semibold'>{item.amount}</h4>
-
-          </div>
-        ))}
+          return( <motion.div
+          variants={variants}
+          initial="hidden"
+          animate={"visible"}
+          transition={{duration:5}}
+          custom={pos}
+      
+          key={item.id} className='relative'>
+            <CardDefined {...item}/>
+          </motion.div>)
         
-      </div>
+})}
+        
+       
+        
+    </div>
     </div>
   )
 }
 
 export default InnerCard3
+
+
+const CardDefined=({text,time,amount})=>{
+  return(
+    <div className='absolute w-60 h-15 shadow-weird flex gap-4 justify-around mx-5 items-center rounded-xl font-sans text-sm bg-white'>
+
+            <div className='flex gap-4 items-center'>
+              <div className='w-10 h-10 bg-lime-500 rounded-full'></div>
+
+            <div className='flex flex-col gap-2 items-center'>
+              <h4 className='font-medium'>{text}</h4>
+              <h2 className='text-xs font-normal text-gray-500'>{time}</h2>
+            </div>
+              
+            </div>
+
+            <h4 className='font-semibold'>{amount}</h4>
+
+          </div>
+  )
+}
